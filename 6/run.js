b58aa55a00988e2680c8cdc996a249ff5a1cf2f8
Cezar Sa Espinola <cezarsa@gmail.com>
2020-12-06 14:34:38 -0300

    day 6 - part 2

--- 6/run.js
+++ 6/run.js
@@ -24,4 +24,31 @@ async function part1() {
     console.log(count)
 }
 
+function intersection(sets) {
+    return sets.reduce((acc, v) => new Set([...acc].filter(x => v.has(x))));
+}
+
+async function part2() {
+    const rl = readline.createInterface({
+        input: fs.createReadStream('input'),
+    });
+
+    var sets = [], count = 0;
+    for await (const line of rl) {
+        if (line == '') {
+            count += intersection(sets).size
+            sets = []
+            continue
+        }
+        let s = new Set()
+        for (var c of line) {
+            s.add(c)
+        }
+        sets.push(s)
+    }
+    count += intersection(sets).size
+    console.log(count)
+}
+
 part1();
+part2();
